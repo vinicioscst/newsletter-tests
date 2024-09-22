e90aaa1ea601ea0d2ee668aae8ed1082cc7029a6
Vinícios Costa <dev.vinicios@gmail.com>
2024-09-22 03:19:55 -0300

    feat: enables env variables to be accessed by cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const dotenvPlugin = require('cypress-dotenv')
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+      config = dotenvPlugin(config)
+      return config
+    }
+  }
+})
